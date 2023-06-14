373514a7ed33e68dde52e85a71b5b366b8e7fdad
Angelica <lisoveckaya1997@gmail.com>
2023-06-14 23:07:29 +0300

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -11,7 +11,7 @@ export const UserMenu = () => {
 
   return (
     <div className={css.wrapper}>
-      <p className={css.username}>Welcome, {user.name}</p>
+      <p className={css.username}>{user.email}</p>
       <button
         className={css.btn}
         type="button"
